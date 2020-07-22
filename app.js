7f519ee4c97287112dbfdbd775bfdf76986616ac
Ushasri Mavuri <ushasrimavuri1999@gmail.com>
2020-07-22 13:55:51 +0530

    added email/password valid re

--- app.js
+++ app.js
@@ -19,12 +19,41 @@ function signup() {
 var username = document.getElementById('username');
 var email1 = document.getElementById('email1');
 var password1 = document.getElementById('password1');
+// validating email address
+function validateEmail(inputEmail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputEmail.match(mailformat)) {
+        return true;
+    } else {
+
+        return false;
+    }
+}
+// validating password
+function validatePassword(inputPassword) {
+    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    if (inputPassword.match(decimal)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 // storing input from register-form
 function store() {
-    localStorage.setItem('email1', email1.value);
-    localStorage.setItem('password1', password1.value);
-    localStorage.setItem('username', username.value);
+    console.log(email1.value);
+    if (validateEmail(email1.value) && validatePassword(password1.value)) {
+        alert("Successfully registered!");
+        // window.NavigationPreloadManager()
+        localStorage.setItem('email1', email1.value);
+        localStorage.setItem('password1', password1.value);
+        localStorage.setItem('username', username.value);
+
+    } else {
+        alert("You have entered an invalid email address / Password!");
+    }
+
 }
 
 // check if stored data from register-form is equal to entered data in the   login-form
@@ -41,8 +70,9 @@ function check() {
     // check if stored data from register-form is equal to data from login form
     if (email.value == storedEmail && password.value == storedPassword) {
         alert('You are loged in.');
-        window.open("result.html");
+        window.location = "result.html";
     } else {
         alert('Invalid Email or Password!');
+        document.querySelector('form').reset();
     }
 }
